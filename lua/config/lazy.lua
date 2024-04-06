4379c99bb8bfd8d0963efad027d59236aeff0773
Cameron Malone <cameronmkm2@gmail.com>
2024-04-07 06:01:29 +1000

    Add trouble and fix some telescope highlights

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.editor.trouble-v3" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,6 +10,9 @@ return {
       -- source: https://www.reddit.com/r/neovim/comments/18faftd/cmp_border_background_is_changed_all_of_a_sudden/
       vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
       vim.cmd("hi NormalFloat guibg=Normal")
+
+      -- for telescope styling
+      vim.cmd("hi TelescopePromptPrefix guifg=#89ddff")
     end,
   },
 }
