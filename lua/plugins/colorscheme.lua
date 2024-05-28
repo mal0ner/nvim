34a132450a46c1caae8fa3e175804222a29cf6e2
Cameron Malone <cameronmkm2@gmail.com>
2024-05-28 23:34:48 +1000

    clean up colorscheme file

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,32 +2,6 @@ return {
   -- -------------------------------------------------
   --               COLORSCHEME                       |
   -- -------------------------------------------------
-  -- {
-  --   "olivercederborg/poimandres.nvim",
-  --   config = function(_)
-  --     vim.cmd.colorscheme("poimandres")
-  --     -- fix for weird border outlines on cmp-windows
-  --     -- source: https://www.reddit.com/r/neovim/comments/18faftd/cmp_border_background_is_changed_all_of_a_sudden/
-  --     vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
-  --     vim.cmd("hi NormalFloat guibg=Normal")
-  --
-  --     -- for telescope styling
-  --     vim.cmd("hi TelescopePromptPrefix guifg=#89ddff")
-  --   end,
-  -- },
-  -- {
-  --   "projekt0n/github-nvim-theme",
-  --   config = function(_)
-  --     vim.cmd.colorscheme("github_dark_default")
-  --     -- fix for weird border outlines on cmp-windows
-  --     -- source: https://www.reddit.com/r/neovim/comments/18faftd/cmp_border_background_is_changed_all_of_a_sudden/
-  --     vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
-  --     vim.cmd("hi NormalFloat guibg=Normal")
-  --
-  --     -- for telescope styling
-  --     vim.cmd("hi TelescopePromptPrefix guifg=#89ddff")
-  --   end,
-  -- },
   {
     "LazyVim/LazyVim",
     opts = {
